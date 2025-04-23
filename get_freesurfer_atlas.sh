d84204789dd829d01174aefac45c8fccc1f38932
LukasRier <lukasrier@outlook.com>
2025-04-23 11:42:28 +0100

    add multi-atlas selection using list and loop

--- get_freesurfer_atlas.sh
+++ get_freesurfer_atlas.sh
@@ -13,6 +13,7 @@ then
 		echo "			or "
 		echo "       <FS_DIRECTORY>/Glasser52_all_reg_1mm_FS.nii.gz (Glasser 52 region atlas - single 3D volume with region numbers in region voxels)"
 		echo "       <FS_DIRECTORY>/MNI152_T1_1mm_FS.nii.gz (MNI template)"
+		echo " v2.0.0 | Lukas Rier 2025
 		exit 0
 fi 
 export FSLOUTPUTTYPE=NIFTI_GZ
@@ -36,59 +37,77 @@ else
 	sub="$(zenity --forms --add-combo="Subject" --combo-values="$pipe_separated" \
 				--title "Select Subject for Atlas alignment")"
 	
-	atlas_choice="$(zenity --forms --add-combo="Atlas" \
-				--combo-values="aal78|glasser52" \
-				--title "Select Atlas")"
+	atlas_choices="$(zenity --list --checklist \
+				--column="Select" --column="Atlas" \
+				--title "Select Atlas" \
+				TRUE "aal78" \
+				FALSE "glasser52")"
 fi
-echo this is my atlas choice $atlas_choice
+
+echo this is my atlas choice $atlas_choices
+
 ATLAS_REGS_sub="${FS_DIRECTORY}/${sub}/mri/AAL_regions"
 ATLAS_REGS="${FS_DIRECTORY}/AAL_all_reg_1mm_FS.nii.gz"
 
-if [ ! -z $atlas_choice ]; then
-    if [ "$atlas_choice" = "aal78" ]; then
-        echo "using AAL78 atlas"
-    elif [ "$atlas_choice" = "glasser52" ]; then
+if [ -z "$atlas_choices" ]; then
+        echo "Third argument is neither 'glasser52' nor 'aal78'."
+		exit 1
+fi
+
+# Zenity returns a string separated by "|" if multiple selected
+IFS='|' read -ra selected_atlases <<< "$atlas_choices"
+
+ANAT="${FS_DIRECTORY}/${sub}/mri/brain"
+mri_convert $ANAT.mgz $ANAT.nii.gz
+MNI2ANAT="${FS_DIRECTORY}/${sub}/mri/mni2anat"
+MNI_brain="${FS_DIRECTORY}/MNI152_T1_1mm_brain_FS.nii.gz"
+
+for atlas_choice in "${selected_atlases[@]}";
+do
+
+	if [ "$atlas_choice" = "aal78" ]; then
+		echo "using AAL78 atlas"
+	elif [ "$atlas_choice" = "glasser52" ]; then
 		echo "using Glasser52 atlas"
 		ATLAS_REGS_sub="${FS_DIRECTORY}/${sub}/mri/Glasser52_regions"
 		ATLAS_REGS="${FS_DIRECTORY}/Glasser52_all_reg_1mm_FS.nii.gz"
-    else
-        echo "Third argument is neither 'glasser52' nor 'aal78'."
+	fi
+
+	# Check files exist
+	if ! [[ -f "$ANAT.nii" || -f "$ANAT.nii.gz" ]]; then
+		echo "$ANAT doesn't exist!"
 		exit 1
-    fi
-else
-    echo "Assuming AAL78 atlas"
-fi
+	fi
 
-ANAT="${FS_DIRECTORY}/${sub}/mri/brain"
+	if ! [ -f "$ATLAS_REGS" ]; then
+		echo "$ATLAS_REGS doesn't exist!"
+		exit 1
+	fi
 
-mri_convert $ANAT.mgz $ANAT.nii.gz
+	if ! [ -f "$MNI_brain" ]; then
+		echo "$MNI_brain doesn't exist!"
+		exit 1
+	fi
 
-MNI2ANAT="${FS_DIRECTORY}/${sub}/mri/mni2anat"
+	if [[ -f "$ATLAS_REGS_sub.nii" || -f "$ATLAS_REGS_sub.nii.gz" ]];then
+		zenity --question \
+			--title="Confirm Overwrite" \
+			--text="$ATLAS_REGS_sub already exists. Do you want to overwrite?"
+	fi
 
-MNI_brain="${FS_DIRECTORY}/MNI152_T1_1mm_brain_FS.nii.gz"
+	if [ $? -ne 0 ]; then
+		echo "Not overwriting $ATLAS_REGS_sub"
+		continue
+	fi
 
-# Check files exist
-if ! [[ -f "$ANAT.nii" || -f "$ANAT.nii.gz" ]]; then
-   echo "$ANAT doesn't exist!"
-   exit 1
-fi
-if ! [ -f "$ATLAS_REGS" ]; then
-   echo "$ATLAS_REGS doesn't exist!"
-   exit 1
-fi
+	# flirt anatomical to MNI saving ANAT2MNI transform
+	echo "flirt ANAT -> MNI brain"
+	flirt -in ${MNI_brain} -ref ${ANAT}.nii.gz -out ${MNI2ANAT} -omat ${MNI2ANAT}.mat \
+	-bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear
 
-if ! [ -f "$MNI_brain" ]; then
-   echo "$MNI_brain doesn't exist!"
-   exit 1
-fi
+	#Apply transform to AAL regions
+	echo "warping AAL regions to subject space"
+	flirt -in ${ATLAS_REGS} -applyxfm -init ${MNI2ANAT}.mat -out ${ATLAS_REGS_sub} -paddingsize 0.0 -interp nearestneighbour -ref ${ANAT}
 
-# flirt anatomical to MNI saving ANAT2MNI transform
-echo "flirt ANAT -> MNI brain"
-flirt -in ${MNI_brain} -ref ${ANAT}.nii.gz -out ${MNI2ANAT} -omat ${MNI2ANAT}.mat \
--bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear
-
-#Apply transform to AAL regions
-echo "warping AAL regions to subject space"
-flirt -in ${ATLAS_REGS} -applyxfm -init ${MNI2ANAT}.mat -out ${ATLAS_REGS_sub} -paddingsize 0.0 -interp nearestneighbour -ref ${ANAT}
-
-mri_convert ${ATLAS_REGS_sub}.nii.gz ${ATLAS_REGS_sub}.mgz
+	mri_convert ${ATLAS_REGS_sub}.nii.gz ${ATLAS_REGS_sub}.mgz
+done
